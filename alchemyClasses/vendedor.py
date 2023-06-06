41a160f02ac211e8cc99c72a607f6c0d8a2eeb6e
sebastiantoad29 <53270020+sebastiantoad29@users.noreply.github.com>
2023-06-06 03:36:55 -0600

    Add files via upload

---
+++ alchemyClasses/vendedor.py
@@ -0,0 +1,22 @@
+from alchemyClasses.__init__ import db
+
+class Vendedor(db.Model):
+    __tablename__ = 'vendedor'
+    vendedor_id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    apellidoP = db.Column(db.String(50), nullable=False)
+    apellidoM = db.Column(db.String(50), nullable=False)
+    edad = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    telefono = db.Column(db.String(20))
+
+    def __init__(self, nombre, apellidoP, apellidoM, edad, email, password, telefono):
+        self.nombre = nombre
+        self.apellidoP = apellidoP
+        self.apellidoM = apellidoM
+        self.edad = edad
+        self.email = email
+        self.password = password
+        self.telefono = telefono
+
